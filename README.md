cf1182d8e1c579000e7d608dfbea11ba0a5f06ca
Ian Blackburn <dogtowncross@yahoo.co.uk>
2012-08-31 17:30:10 +0100

    66-76 - SurfaceView GFX Animation Thread Updated README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+The New Boston Android Tutorials
+
+Next up: Lesson 77
+Carry on from here:
+http://www.youtube.com/watch?v=S36C23lW5qI&feature=autoplay&list=PL3D7BFF1DDBDAAFE5&playnext=11
--- src/com/thenewboston/travis/Menu.java
+++ src/com/thenewboston/travis/Menu.java
@@ -13,7 +13,7 @@ import android.widget.ListView;
 
 public class Menu extends ListActivity {
 
-	String classes[] = {"startingPoint", "TextPlay", "Email", "Camera", "Data", "GFX", "example7"};
+	String classes[] = {"startingPoint", "TextPlay", "Email", "Camera", "Data", "GFX", "GFXSurface"};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
